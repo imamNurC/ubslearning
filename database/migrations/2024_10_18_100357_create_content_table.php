5584520eb67bc68bc2c3ea6b22a0514411d56f2c
Varestha <ican9943@gmail.com>
2024-10-18 20:52:12 +0700

    perubahan lagi

--- database/migrations/2024_10_18_100357_create_content_table.php
+++ database/migrations/2024_10_18_100357_create_content_table.php
@@ -36,7 +36,7 @@ return new class extends Migration
         });
 
         Schema::create('access', function (Blueprint $table) {
-            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('id_customer');
             $table->unsignedBigInteger('id_content');
             $table->timestamps();
 
