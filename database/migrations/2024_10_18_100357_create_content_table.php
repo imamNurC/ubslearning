06e587d87bf155206107fd513f174970185c4fdb
Varestha <ican9943@gmail.com>
2024-10-18 20:40:33 +0700

    ada perubahan

--- database/migrations/2024_10_18_100357_create_content_table.php
+++ database/migrations/2024_10_18_100357_create_content_table.php
@@ -14,35 +14,35 @@ return new class extends Migration
         Schema::create('content', function (Blueprint $table) {
             $table->id('id_content');
             $table->string('content_name', 100);
-            $table->integer('price', 20);
+            $table->integer('price');
             $table->string('youtube_url');
             $table->timestamps();
         });
 
         Schema::create('transaction', function (Blueprint $table) {
-            $table->id('id_transaction');
-            $table->unsignedBigInteger('user_id');
+            $table->id('id_transaction'); 
+            $table->unsignedBigInteger('id_customer'); 
             $table->string('name');
             $table->string('number_phone', 15);
             $table->string('email');
-            $table->unsignedBigInteger('content_id');
+            $table->unsignedBigInteger('id_content');
             $table->string('content_name', 100);
-            $table->integer('price', 20);
+            $table->integer('price');
             $table->timestamps();
             
             //relasi
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('content_id')->references('id_content')->on('content')->onDelete('cascade');
+            $table->foreign('id_customer')->references('id_customer')->on('customers')->onDelete('cascade');
+            $table->foreign('id_content')->references('id_content')->on('content')->onDelete('cascade');
         });
 
         Schema::create('access', function (Blueprint $table) {
             $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('content_id');
+            $table->unsignedBigInteger('id_content');
             $table->timestamps();
 
             //relasi
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('content_id')->references('conten_id')->on('content')->onDelete('cascade');
+            $table->foreign('id_customer')->references('id_customer')->on('customers')->onDelete('cascade');
+            $table->foreign('id_content')->references('id_content')->on('content')->onDelete('cascade');
         });
     }
 
@@ -52,5 +52,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('content');
+        Schema::dropIfExists('transaction');
+        Schema::dropIfExists('access');
     }
 };
