707609de5c57975c9b348e8e1890f9f464abec9c
MuhammadPikih <muhammadpikih296@gmail.com>
2024-10-24 16:34:49 +0700

    "Updated routes in web.php: changed root route to return 'sidebar_admin/sidebar' view, removed '/sidebar' route"

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('sidebar_admin/sidebar');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard/dashboard');
 });
 
-Route::get('/sidebar', function () {
-    return view('sidebar_admin/sidebar');
-});
+
