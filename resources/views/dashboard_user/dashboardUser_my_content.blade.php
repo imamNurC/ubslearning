f6e751c66ffc5a7d6418e94b69f2a78d39ed2b1a
Fanzeer <ikhwanudinirfan12@gmail.com>
2024-10-26 14:30:29 +0700

    fitur user

--- resources/views/dashboard_user/dashboardUser_my_content.blade.php
+++ resources/views/dashboard_user/dashboardUser_my_content.blade.php
@@ -1,4 +1,283 @@
 @extends('custom_layout_users.bases.userbase')
 @section('content')
-<h5> Ini Menu My Content </h5>
+
+<body class="font-poppins bg-gray-50">
+    <div class="container mx-auto px-4 py-4">
+        <h2 class="text-center text-3xl font-bold mb-4 "> Daftar Konten Terbeli</h2>
+    <div class="flex flex-col mt-4">
+        <div class="py-2 -my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+            <div class="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg">
+                <table class="min-w-full">
+                    <thead>
+                        <tr>
+                            <th class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                                Title</th>
+                            <th class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                                Instructor</th>
+                            <th class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                                Status</th>
+                            <th class="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
+                        </tr>
+                    </thead>
+
+                    <tbody class="bg-white">
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="flex items-center">
+                                    <div class="flex-shrink-0 w-10 h-10">
+                                        <img class="w-10 h-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="">
+                                    </div>
+
+                                    <div class="ml-4">
+                                        <div class="text-sm font-medium leading-5 text-gray-900">HTML & CSS
+                                        </div>
+                                        <div class="text-sm leading-5 text-gray-500">Coding</div>
+                                    </div>
+                                </div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="text-sm leading-5 text-gray-900">Tukang Ager</div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <span class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">Active</span>
+                            </td>
+
+                            <td class="px-6 py-4 text-sm font-medium leading-5 text-right whitespace-no-wrap border-b border-gray-200">
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Play</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="flex items-center">
+                                    <div class="flex-shrink-0 w-10 h-10">
+                                        <img class="w-10 h-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="">
+                                    </div>
+
+                                    <div class="ml-4">
+                                        <div class="text-sm font-medium leading-5 text-gray-900">HTML & CSS
+                                        </div>
+                                        <div class="text-sm leading-5 text-gray-500">Coding</div>
+                                    </div>
+                                </div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="text-sm leading-5 text-gray-900">Tukang Ager</div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <span class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">Active</span>
+                            </td>
+
+                            <td class="px-6 py-4 text-sm font-medium leading-5 text-right whitespace-no-wrap border-b border-gray-200">
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Play</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="flex items-center">
+                                    <div class="flex-shrink-0 w-10 h-10">
+                                        <img class="w-10 h-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="">
+                                    </div>
+
+                                    <div class="ml-4">
+                                        <div class="text-sm font-medium leading-5 text-gray-900">HTML & CSS
+                                        </div>
+                                        <div class="text-sm leading-5 text-gray-500">Coding</div>
+                                    </div>
+                                </div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="text-sm leading-5 text-gray-900">Tukang Ager</div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <span class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">Active</span>
+                            </td>
+
+                            <td class="px-6 py-4 text-sm font-medium leading-5 text-right whitespace-no-wrap border-b border-gray-200">
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Play</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="flex items-center">
+                                    <div class="flex-shrink-0 w-10 h-10">
+                                        <img class="w-10 h-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="">
+                                    </div>
+
+                                    <div class="ml-4">
+                                        <div class="text-sm font-medium leading-5 text-gray-900">HTML & CSS
+                                        </div>
+                                        <div class="text-sm leading-5 text-gray-500">Coding</div>
+                                    </div>
+                                </div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="text-sm leading-5 text-gray-900">Tukang Ager</div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <span class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">Active</span>
+                            </td>
+
+                            <td class="px-6 py-4 text-sm font-medium leading-5 text-right whitespace-no-wrap border-b border-gray-200">
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Play</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="flex items-center">
+                                    <div class="flex-shrink-0 w-10 h-10">
+                                        <img class="w-10 h-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="">
+                                    </div>
+
+                                    <div class="ml-4">
+                                        <div class="text-sm font-medium leading-5 text-gray-900">HTML & CSS
+                                        </div>
+                                        <div class="text-sm leading-5 text-gray-500">Coding</div>
+                                    </div>
+                                </div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="text-sm leading-5 text-gray-900">Tukang Ager</div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <span class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">Active</span>
+                            </td>
+
+                            <td class="px-6 py-4 text-sm font-medium leading-5 text-right whitespace-no-wrap border-b border-gray-200">
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Play</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="flex items-center">
+                                    <div class="flex-shrink-0 w-10 h-10">
+                                        <img class="w-10 h-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="">
+                                    </div>
+
+                                    <div class="ml-4">
+                                        <div class="text-sm font-medium leading-5 text-gray-900">HTML & CSS
+                                        </div>
+                                        <div class="text-sm leading-5 text-gray-500">Coding</div>
+                                    </div>
+                                </div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="text-sm leading-5 text-gray-900">Tukang Ager</div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <span class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">Active</span>
+                            </td>
+
+                            <td class="px-6 py-4 text-sm font-medium leading-5 text-right whitespace-no-wrap border-b border-gray-200">
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Play</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="flex items-center">
+                                    <div class="flex-shrink-0 w-10 h-10">
+                                        <img class="w-10 h-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="">
+                                    </div>
+
+                                    <div class="ml-4">
+                                        <div class="text-sm font-medium leading-5 text-gray-900">HTML & CSS
+                                        </div>
+                                        <div class="text-sm leading-5 text-gray-500">Coding</div>
+                                    </div>
+                                </div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="text-sm leading-5 text-gray-900">Tukang Ager</div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <span class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">Active</span>
+                            </td>
+
+                            <td class="px-6 py-4 text-sm font-medium leading-5 text-right whitespace-no-wrap border-b border-gray-200">
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Play</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="flex items-center">
+                                    <div class="flex-shrink-0 w-10 h-10">
+                                        <img class="w-10 h-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="">
+                                    </div>
+
+                                    <div class="ml-4">
+                                        <div class="text-sm font-medium leading-5 text-gray-900">HTML & CSS
+                                        </div>
+                                        <div class="text-sm leading-5 text-gray-500">Coding</div>
+                                    </div>
+                                </div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="text-sm leading-5 text-gray-900">Tukang Ager</div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <span class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">Active</span>
+                            </td>
+
+                            <td class="px-6 py-4 text-sm font-medium leading-5 text-right whitespace-no-wrap border-b border-gray-200">
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Play</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+        
+
+
+
+
+    <div class=" mt-2 px-6 max-w-full bg-gray-900">
+    <div class="flex justify-center mb-6">
+        <div class="text-center sm:w-1/3">
+            <img src="image/Malaz_Solutions.png" alt="logo" class="w-32 bg-white mt-1 rounded-md">
+            <p class="text-left text-white">PT. PT Ceban </br>
+            Jl. Jalanin Aja Dulu No.99, Desa Gacor, Zeus, Bekasi 17510 
+            </p>
+        </div>
+        <div class="text-center sm:w-1/3">
+            <h3 class="text-white text-lg font-semibold mb-2">Pusat Bantuan</h3>
+            <ul class="space-y-2">
+                <li class="text-white hover:text-red-500 cursor-pointer font-semibold">FAQ</li>
+                <li class="text-white hover:text-red-500 cursor-pointer font-semibold">Hubungi Kami</li>
+                <li class="text-white hover:text-red-500 cursor-pointer font-semibold">Kebijakan Privasi</li>
+                <li class="text-white hover:text-red-500 cursor-pointer font-semibold">Syarat & Ketentuan</li>
+            </ul>
+        </div>
+        <div class="text-center sm:w-1/3">
+            <h3 class="text-white text-lg font-semibold mb-2">Follow us</h3>
+            <ul class="space-y-2">
+                <li class="text-white hover:text-red-500 cursor-pointer font-semibold">Facebook</li>
+                <li class="text-white hover:text-red-500 cursor-pointer font-semibold">Twitter</li>
+                <li class="text-white hover:text-red-500 cursor-pointer font-semibold">Instagram</li>
+                <li class="text-white hover:text-red-500 cursor-pointer font-semibold">YouTube</li>
+            </ul>
+        </div>
+    </div>
+    <hr class="border-gray-400 w-full">
+    <p class="text-center text-gray-200">Copyright &copy; 2024 - Malaz Solutions</p>
+    </div>
+</body>
+
 @endsection
