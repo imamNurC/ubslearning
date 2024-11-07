92051ab5a84f290dc7e3e82bb3e8f993e6d232c8
Varestha <ican9943@gmail.com>
2024-11-07 17:40:33 +0700

    mantap lah

--- resources/views/dashboard_user/dashboardUser_my_profile.blade.php
+++ resources/views/dashboard_user/dashboardUser_my_profile.blade.php
@@ -18,15 +18,15 @@
         <div class="bg-white overflow-hidden shadow rounded-lg">
             <div class="px-4 py-5 sm:px-6 flex items-center">
                 <!-- Foto Profil -->
-                <img src="{{ Storage::url($customer->image_path) }}" alt="Profile Picture" class="h-12 w-12 rounded-full mr-4">
+                <img src="{{ Storage::url($customer->image_path) }}" alt="Profile Picture" class="object-cover h-12 w-12 rounded-full mr-4">
                 
                 <!-- Judul dan Deskripsi -->
                 <div>
                     <h3 class="text-lg leading-6 font-medium text-gray-900">
-                        User Profile
+                        {{ $customer->username }}
                     </h3>
                     <p class="mt-1 max-w-2xl text-sm text-gray-500">
-                        This is some information about the user.
+                        This is some information about you
                     </p>
                 </div>
             </div>
@@ -47,14 +47,12 @@
                     </div>
                     <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                         <dt class="text-sm font-medium text-gray-500">Password</dt>
-                        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex justify-between items-center">
                             <span id="password-display">{{ str_repeat('*', strlen($customer->password)) }}</span>
-                        </dd>
-                        <!-- Tombol Change Password -->
-                        <div class="sm:col-span-3 mt-2 text-right">
+                            <!-- Tombol Change Password -->
                             <button type="button" class="bg-blue-500 text-white px-4 py-2 rounded" onclick="openChangePassword()">Change Password</button>
-                        </div>
-                    </div>
+                        </dd>
+                    </div>                    
                 </dl>
                 
                 <div class="mt-20 flex justify-center">
@@ -86,7 +84,7 @@
                             Choose File
                         </label>
                         <input type="file" id="image" name="image" class="hidden" onchange="previewImage(event)">
-                        <img id="profile-preview" src="#" alt="Profile Picture" class="rounded-full w-24 h-24 mt-4" style="display: none;">
+                        <img id="profile-preview" src="#" alt="Profile Picture" class="object-cover rounded-full w-24 h-24 mt-4" style="display: none;">
                     </div>
                 </div>
                 
